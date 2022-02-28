05afd68264b9298cbd8afbb091ea54022eb1aa24
jlthornton3 <91161036+jlthornton3@users.noreply.github.com>
2022-02-27 20:42:01 -1000

    database

--- config/Database.php
+++ config/Database.php
@@ -4,14 +4,18 @@
         private $host = 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         private $dbname = 'sikqs5dxp6xtbo78';
         private $username = 'naz3cbsk74out143';
-        private $password = getenv('mysqlpwd');
+        //private $password = getenv('mysqlpwd');
+        //private $host = 'localhost';
+        //private $dbname = 'myblog';
+        //private $username = 'root';
+        //private $password = '';
         private $conn;
 
         //DB Connect
         public function connect() {
             $this->conn = null;
             try{
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname='. $this->dbname, $this->username, $this->password);
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname='. $this->dbname, $this->username, getenv('mysqlpwd'));
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }catch(PDOException $e){
                 echo 'Connection Error: ' . $e->getMessage();
