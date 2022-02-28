7ccfe92cd7e3bd8492b19dd479e3fc7458ce2335
jlthornton3 <91161036+jlthornton3@users.noreply.github.com>
2022-02-27 20:23:11 -1000

    database update

--- config/Database.php
+++ config/Database.php
@@ -1,9 +1,9 @@
 <?php
     class Database {
         //DB Params
-        private $host = 'localhost';
-        private $dbname = 'myblog';
-        private $username = 'root';
+        private $host = 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        private $dbname = 'sikqs5dxp6xtbo78';
+        private $username = 'naz3cbsk74out143';
         private $password = getenv('mysqlpwd');
         private $conn;
 
