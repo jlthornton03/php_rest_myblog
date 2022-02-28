4429c0592235b743508932faccd34f3abaea175b
jlthornton3 <91161036+jlthornton3@users.noreply.github.com>
2022-02-27 20:46:53 -1000

    Database update again

--- config/Database.php
+++ config/Database.php
@@ -8,14 +8,18 @@
         //private $host = 'localhost';
         //private $dbname = 'myblog';
         //private $username = 'root';
-        //private $password = '';
+        private $password = '';
         private $conn;
 
         //DB Connect
+        function __construct() {
+            $this->password = getenv('mysqlpwd');
+        }
+
         public function connect() {
             $this->conn = null;
             try{
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname='. $this->dbname, $this->username, getenv('mysqlpwd'));
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname='. $this->dbname, $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }catch(PDOException $e){
                 echo 'Connection Error: ' . $e->getMessage();
